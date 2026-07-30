77ae6e135decabc031d2996d4bffa91b32b6134f
Pedro Henrick <pedro.h.santos507@aluno.senai.br>
2026-07-30 15:11:05 -0300

    fiz o css do arquivo lista-produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-produtos',
+  standalone: true,
   imports: [],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
