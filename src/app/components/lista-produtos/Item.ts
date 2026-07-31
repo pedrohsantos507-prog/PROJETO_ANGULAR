c7252613cd729600a464143c88a34fbaf6964bef
Pedro Henrick <pedro.h.santos507@aluno.senai.br>
2026-07-31 12:36:14 -0300

    fiz alterações nos arquivos

--- src/app/components/lista-produtos/Item.ts
+++ src/app/components/lista-produtos/Item.ts
@@ -2,7 +2,10 @@ export class Item{
     idProduto?: number
     descricaoProduto?: string
     valorUnitario?: number
+
     statusItem: boolean = false 
+    selecionado: boolean = false 
+
 /*
 CONSTRUTOR DA CLASSE !
 
--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,35 +1,41 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '../../../../node_modules/@angular/forms/types/forms';
-import { Item } from './item'
+import { FormsModule } from '@angular/forms';
+import { Item } from './Item';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [ FormsModule ],
+  imports: [FormsModule],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  descricao_produto : string = 'valor'
-  valor_unitario : number = 0.0
-  listaItens : Item[] = []
+  descricao_produto: string = '';
+  valor_unitario: number | null = null;
+  listaItens: Item[] = [];
 
-
-  addItem(){
+  addItem() {
     //INSTANCIA DA CLASSE ITEM PASSANDO OS PARÂMETROS DO CONSTRUTOR
     //let item = new Item(this.descricao_produto, this.valor_unitario)
-    
+
     //instancia da classe item (criando o objeto item)
-    let item = new Item()
-    item.idProduto = this.listaItens.length + 1
-    item.descricaoProduto = this.descricao_produto
-    item.valorUnitario = this.valor_unitario
+    let item = new Item();
+    item.idProduto = this.listaItens.length + 1;
+    item.descricaoProduto = this.descricao_produto;
+    item.valorUnitario = this.valor_unitario ?? 0;
 
     //adicionando objeto item ao array listaItens
-    this.listaItens.push(item)
-  
+    this.listaItens.push(item);
+
     //limpando as propriedades da classe
-    this.descricao_produto = ''
-    this.valor_unitario = 0.0
+    this.descricao_produto = '';
+    this.valor_unitario = null;
   }
-  
+
+  selecionarItem(item: Item) {
+    item.selecionado = !item.selecionado;
+  }
+
+  limparTudo() {
+    this.listaItens = [];
+  }
 }
